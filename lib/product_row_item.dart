387f518230914fc178864d91c2338e92eabc0877
Huy Phong <phong.bnh@globalfactories.com>
2021-01-07 17:58:38 +0700

    update package imgs

---
+++ lib/product_row_item.dart
@@ -0,0 +1,86 @@
+import 'package:cupertino_app/models/app_state_model.dart';
+import 'package:cupertino_app/models/product.dart';
+import 'package:cupertino_app/style.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+class ProductRowItem extends StatelessWidget {
+  final Product product;
+  final int index;
+  final bool lastItem;
+
+  const ProductRowItem({this.index, this.product, this.lastItem});
+
+  @override
+  Widget build(BuildContext context) {
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      minimum: const EdgeInsets.only(left: 16, top: 8, bottom: 8, right: 8),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Image.asset(
+              product.assetName,
+              package: product.assetPackage,
+              fit: BoxFit.cover,
+              width: 76,
+              height: 76,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    product.name,
+                    style: Styles.productRowItemName,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 8),
+                  ),
+                  Text(
+                    '\$${product.price}',
+                    style: Styles.productRowItemPrice,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              final model = Provider.of<AppStateModel>(context, listen: false);
+              model.addProductToCart(product.id);
+            },
+            child: const Icon(
+              CupertinoIcons.plus_circle,
+              semanticLabel: 'Add',
+            ),
+          )
+        ],
+      ),
+    );
+
+    if (lastItem) {
+      return row;
+    }
+
+    return Column(
+      children: <Widget>[
+        row,
+        Padding(
+          padding: const EdgeInsets.only(left: 100, right: 16),
+          child: Container(
+            height: 1,
+            color: Styles.productRowDivider,
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/style.dart
+++ lib/style.dart
@@ -12,4 +12,12 @@ abstract class Styles {
       fontSize: 18,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.normal);
+
+  static const TextStyle productRowItemPrice = TextStyle(
+    color: Color(0xFF8E8E93),
+    fontSize: 13,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const Color productRowDivider = Color(0xFFD9D9D9);
 }
