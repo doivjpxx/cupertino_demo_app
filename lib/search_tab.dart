8c82f6fc61d5d82d5a8255f22054cd4f69a2b767
Huy Phong <doivjpxx@gmail.com>
2021-01-07 21:55:37 +0700

    complete shopping cart

--- lib/search_tab.dart
+++ lib/search_tab.dart
@@ -1,4 +1,10 @@
+import 'package:cupertino_app/search_bar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'models/app_state_model.dart';
+import 'product_row_item.dart';
+import 'style.dart';
 
 class SearchTab extends StatefulWidget {
   @override
@@ -6,14 +12,63 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+  TextEditingController _controller;
+  FocusNode _focusNode;
+  String _terms = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController()..addListener(_onTextChanged);
+    _focusNode = FocusNode();
+  }
+
+  void _onTextChanged() {
+    setState(() {
+      _terms = _controller.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Search'),
-        )
-      ],
+    final model = Provider.of<AppStateModel>(context);
+    final results = model.search(_terms);
+    return DecoratedBox(
+        decoration: const BoxDecoration(color: Styles.scaffoldBackground),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              _buildSearchBox(),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return ProductRowItem(
+                      index: index,
+                      product: results[index],
+                      lastItem: index == results.length - 1,
+                    );
+                  },
+                  itemCount: results.length,
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    _focusNode.dispose();
+  }
+}
+
+extension MoreSearchTab on _SearchTabState {
+  Widget _buildSearchBox() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SearchBar(controller: _controller, focusNode: _focusNode),
     );
   }
 }
--- lib/shopping_cart_tab.dart
+++ lib/shopping_cart_tab.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import 'models/app_state_model.dart';
+import 'style.dart';
+
+const double _kDateTimePickerHeight = 216;
 
 class ShoppingCartTab extends StatefulWidget {
   @override
@@ -9,18 +13,166 @@ class ShoppingCartTab extends StatefulWidget {
 }
 
 class _ShoppingCartTabState extends State<ShoppingCartTab> {
+  String name;
+  String email;
+  String location;
+  String pin;
+  DateTime dateTime = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
       builder: (context, model, child) {
-        return const CustomScrollView(
+        return CustomScrollView(
           slivers: <Widget>[
             CupertinoSliverNavigationBar(
               largeTitle: Text('Shopping Cart'),
-            )
+            ),
+            SliverSafeArea(
+                top: false,
+                minimum: const EdgeInsets.only(top: 4),
+                sliver: SliverList(
+                    delegate: _buildSliverChildBuilderDelegate(model))),
           ],
         );
       },
+    );
+  }
+}
+
+extension MoreShoppingCartTab on _ShoppingCartTabState {
+  Widget _buildNameField() {
+    return CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.person_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 12.0),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+          border: Border(
+              bottom:
+                  BorderSide(width: 0, color: CupertinoColors.inactiveGray))),
+      placeholder: 'Name',
+      onChanged: (newName) {
+        setState(() {
+          name = newName;
+        });
+      },
+    );
+  }
+
+  Widget _buildEmailField() {
+    return const CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.mail_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 12.0),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+          border: Border(
+              bottom:
+                  BorderSide(width: 0, color: CupertinoColors.inactiveGray))),
+      placeholder: 'Email',
+    );
+  }
+
+  Widget _buildLocationField() {
+    return const CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.location_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 12.0),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+          border: Border(
+              bottom:
+                  BorderSide(width: 0, color: CupertinoColors.inactiveGray))),
+      placeholder: 'Location',
+    );
+  }
+
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
+      AppStateModel model) {
+    return SliverChildBuilderDelegate((context, index) {
+      switch (index) {
+        case 0:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: _buildNameField(),
+          );
+        case 1:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: _buildEmailField(),
+          );
+        case 2:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: _buildLocationField(),
+          );
+        case 3:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: _buildDateAndTimePicker(context),
+          );
+
+        default:
+      }
+      return null;
+    });
+  }
+
+  Widget _buildDateAndTimePicker(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: [
+                Icon(
+                  CupertinoIcons.clock,
+                  color: CupertinoColors.lightBackgroundGray,
+                  size: 28,
+                ),
+                SizedBox(
+                  width: 6.0,
+                ),
+                Text(
+                  'Delivery time',
+                  style: Styles.deliveryTimeLabel,
+                )
+              ],
+            ),
+            Text(
+              DateFormat.yMMMd().add_jm().format(dateTime),
+              style: Styles.deliveryTime,
+            )
+          ],
+        ),
+        Container(
+          height: _kDateTimePickerHeight,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.dateAndTime,
+            initialDateTime: dateTime,
+            onDateTimeChanged: (newDateTime) {
+              setState(() {
+                dateTime = newDateTime;
+              });
+            },
+          ),
+        )
+      ],
     );
   }
 }
