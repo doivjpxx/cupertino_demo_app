094bcb033a4f44ff5ef95335be4bd1934cba7e63
Huy Phong <phong.bnh@globalfactories.com>
2021-01-07 16:49:12 +0700

    first commit

---
+++ lib/shopping_cart_tab.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'models/app_state_model.dart';
+
+class ShoppingCartTab extends StatefulWidget {
+  @override
+  _ShoppingCartTabState createState() => _ShoppingCartTabState();
+}
+
+class _ShoppingCartTabState extends State<ShoppingCartTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateModel>(
+      builder: (context, model, child) {
+        return const CustomScrollView(
+          slivers: <Widget>[
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('Shopping Cart'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
